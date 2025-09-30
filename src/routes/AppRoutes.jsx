025457f4d8f873880ed9ffc3025c08bafa756b6c
rushikesh-dataintelo <rushikesh.sonar@dataintelo.com>
2025-09-30 14:40:52 +0530

    Common folders creaded

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function AppRoutes() {
+    return (
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+    );
+}
